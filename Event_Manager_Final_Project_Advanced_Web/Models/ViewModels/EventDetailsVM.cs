433c3b17cf84e5674bd170c760fb59b282691aa1
Peyton Minnick <minnickp@etsu.edu>
2025-05-03 09:44:52 -0400

    Uploaded View Models
    Uploaded Initial View Models for UserDetailsVM and EventDetailsVM

---
+++ Event_Manager_Final_Project_Advanced_Web/Models/ViewModels/EventDetailsVM.cs
@@ -0,0 +1,14 @@
+﻿namespace Event_Manager_Final_Project_Advanced_Web.Models.ViewModels
+{
+    public class EventDetailsVM
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime EventTime { get; set; }
+        public string? Location { get; set; }
+        public int CreatedByUser { get; set; }
+        public int UserCount {  get; set; }
+
+    }
+}
---
+++ Event_Manager_Final_Project_Advanced_Web/Models/ViewModels/UserDetailsVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Event_Manager_Final_Project_Advanced_Web.Models.ViewModels
+{
+    public class UserDetailsVM
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? UserEmail { get; set; }
+        public string? Password { get; set; }
+        public int EventCount { get; set; }
+    }
+}
