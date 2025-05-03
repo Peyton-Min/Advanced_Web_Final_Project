0dbdf2a6e6dd38c71afa91cf07cf39c8d2ba2639
Peyton Minnick <minnickp@etsu.edu>
2025-05-03 09:43:40 -0400

    Uploaded Entities
    Uploaded three entity files, Event, User, and EventParticipant.

---
+++ Event_Manager_Final_Project_Advanced_Web/Models/Entities/EventParticipant.cs
@@ -0,0 +1,15 @@
+﻿namespace Event_Manager_Final_Project_Advanced_Web.Models.Entities
+{
+    public class EventParticipant
+    {
+        public int Id { get; set; }
+
+        public int EventId { get; set; }
+        public Event? Event { get; set; }
+
+        public int Userint { get; set; }
+        public User? User { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+    }
+}
---
+++ Event_Manager_Final_Project_Advanced_Web/Models/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Event_Manager_Final_Project_Advanced_Web.Models.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? UserEmail { get; set; }
+        public string? Password { get; set; }
+        public ICollection<EventParticipant>? EventParticipants { get; set; }
+    }
+}
