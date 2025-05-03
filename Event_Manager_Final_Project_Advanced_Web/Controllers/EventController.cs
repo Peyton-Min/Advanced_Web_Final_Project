295bc10ce59898d710dff447c83a7435acdbe2bc
Peyton Minnick <minnickp@etsu.edu>
2025-05-03 16:51:01 -0400

    Implemented CRUD functionality
    Implemented CRUD functionality in Controllers for Events and Users

--- Event_Manager_Final_Project_Advanced_Web/Controllers/EventController.cs
+++ Event_Manager_Final_Project_Advanced_Web/Controllers/EventController.cs
@@ -1,4 +1,5 @@
 ﻿using Event_Manager_Final_Project_Advanced_Web.Models.ViewModels;
+using Event_Manager_Final_Project_Advanced_Web.Models.Entities;
 using Event_Manager_Final_Project_Advanced_Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +29,68 @@ namespace Event_Manager_Final_Project_Advanced_Web.Controllers
                 UserCount = b.EventParticipants?.Count ?? 0
             });
             return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Event ev)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ev);
+            }
+
+            await _eventRepository.CreateAsync(ev);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var ev = await _eventRepository.ReadAsync(id);
+
+            if (ev == null)
+            {
+                return NotFound();
+            }
+
+            return View(ev);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Event ev)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ev);
+            }
+
+            await _eventRepository.UpdateAsync(ev);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var ev = await _eventRepository.ReadAsync(id);
+            if (ev == null)
+            {
+                return NotFound();
+            }
+
+            return View(ev);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _eventRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Event_Manager_Final_Project_Advanced_Web/Controllers/UserController.cs
+++ Event_Manager_Final_Project_Advanced_Web/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Event_Manager_Final_Project_Advanced_Web.Models.ViewModels;
+using Event_Manager_Final_Project_Advanced_Web.Models.Entities;
 using Event_Manager_Final_Project_Advanced_Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +26,71 @@ namespace Event_Manager_Final_Project_Advanced_Web.Controllers
                 EventCount = b.EventParticipants?.Count ?? 0
             });
             return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            await _userRepository.CreateAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var user = await _userRepository.ReadAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            await _userRepository.UpdateAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userRepository.ReadAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // 
+        //
+        // Source: https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/examining-the-details-and-delete-methods
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _userRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
