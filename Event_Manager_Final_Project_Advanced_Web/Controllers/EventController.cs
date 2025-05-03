96c7c9b785bbc84279408d904f0dacc00833e913
Peyton Minnick <minnickp@etsu.edu>
2025-05-03 13:53:24 -0400

    Update Controllers
    Added ReadAllAsync into controllers

---
+++ Event_Manager_Final_Project_Advanced_Web/Controllers/EventController.cs
@@ -0,0 +1,33 @@
+﻿using Event_Manager_Final_Project_Advanced_Web.Models.ViewModels;
+using Event_Manager_Final_Project_Advanced_Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Manager_Final_Project_Advanced_Web.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public EventController(IEventRepository eventRepo)
+        {
+            _eventRepository = eventRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var events = await _eventRepository.ReadAllAsync();
+
+            var vm = events.Select(b => new EventDetailsVM
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Description = b.Description,
+                EventTime = b.EventTime,
+                Location = b.Location,
+                CreatedByUser = b.CreatedByUser,
+                UserCount = b.EventParticipants?.Count ?? 0
+            });
+            return View(vm);
+        }
+    }
+}
---
+++ Event_Manager_Final_Project_Advanced_Web/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Event_Manager_Final_Project_Advanced_Web.Models.ViewModels;
+using Event_Manager_Final_Project_Advanced_Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Manager_Final_Project_Advanced_Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepo)
+        {
+            _userRepository = userRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.ReadAllAsync();
+
+            var vm = users.Select(b => new UserDetailsVM
+            {
+                Id = b.Id,
+                Username = b.Username,
+                Password = b.Password,
+                EventCount = b.EventParticipants?.Count ?? 0
+            });
+            return View(vm);
+        }
+    }
+}
