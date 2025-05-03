50866eccce10f5eacc1e3c17eddb893a2955aa1d
Peyton Minnick <minnickp@etsu.edu>
2025-05-03 14:17:17 -0400

    Updated DbEventRepo
    Updataed DbEventRepo after accidentally leaving it blank.

--- Event_Manager_Final_Project_Advanced_Web/Services/DbEventRepository.cs
+++ Event_Manager_Final_Project_Advanced_Web/Services/DbEventRepository.cs
@@ -5,5 +5,26 @@ namespace Event_Manager_Final_Project_Advanced_Web.Services
 {
     public class DbEventRepository : IEventRepository
     {
+        private readonly ApplicationDbContext _db;
+
+        public DbEventRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<ICollection<Event>> ReadAllAsync()
+        {
+            return await _db.Events
+            .Include(a => a.EventParticipants)
+            .ToListAsync();
+        }
+
+        public async Task<Event?> ReadAsync(int id)
+        {
+            return await _db.Events
+                .Include(a => a.EventParticipants)
+                .ThenInclude(ba => ba.User)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
     }
 }
