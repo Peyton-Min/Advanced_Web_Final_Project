4a3138f6c8e8d727c86b265446a27be5f4a63dd8
Peyton Minnick <minnickp@etsu.edu>
2025-05-03 10:24:50 -0400

    Final Database Migrations
    Uploaded the Migrations for the final version of the database

--- Event_Manager_Final_Project_Advanced_Web/Migrations/20250503141620_Mig01.Designer.cs
+++ Event_Manager_Final_Project_Advanced_Web/Migrations/20250503141620_Mig01.Designer.cs
@@ -4,6 +4,7 @@ using Event_Manager_Final_Project_Advanced_Web.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Event_Manager_Final_Project_Advanced_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250503141620_Mig01")]
+    partial class Mig01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
