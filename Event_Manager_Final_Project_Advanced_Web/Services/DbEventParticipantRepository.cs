46bb788946c962f3e3e5b9826cb7f0a6a9d6c158
Peyton Minnick <minnickp@etsu.edu>
2025-05-04 20:37:39 -0400

    Uploaded EventparticRepo and IEventParticipant
    Created EventParticipationRepository and IEventParticpation to allow for the fully functional joining events feature.

---
+++ Event_Manager_Final_Project_Advanced_Web/Services/DbEventParticipantRepository.cs
@@ -0,0 +1,34 @@
+﻿using Event_Manager_Final_Project_Advanced_Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Event_Manager_Final_Project_Advanced_Web.Services
+{
+    public class DbEventParticipantRepository : IEventParticipantRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DbEventParticipantRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task CreateAsync(EventParticipant participant)
+        {
+            _db.EventParticipants.Add(participant);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int userId, int eventId)
+        {
+            var record = await _db.EventParticipants
+                .FirstOrDefaultAsync(p => p.Userint == userId && p.EventId == eventId);
+
+            if (record != null)
+            {
+                _db.EventParticipants.Remove(record);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Event_Manager_Final_Project_Advanced_Web/Services/IEventParticipantRepository.cs
@@ -0,0 +1,11 @@
+﻿using Event_Manager_Final_Project_Advanced_Web.Models.Entities;
+
+namespace Event_Manager_Final_Project_Advanced_Web.Services
+{
+    public interface IEventParticipantRepository
+    {
+        //Add read and readall here when get to listing eventparticipants 
+        Task CreateAsync(EventParticipant participant);
+        Task DeleteAsync(int userId, int eventId);
+    }
+}
